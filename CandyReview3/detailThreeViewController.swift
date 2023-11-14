351d559ab236f9a8e5c4b4414ac470585eedab93
linacd <linacd@hawaii.edu>
2023-11-14 12:54:20 -1000

    fixing tables

---
+++ CandyReview3/detailThreeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  detailThreeViewController.swift
+//  CandyReview3
+//
+//  Created by Lina Dacanay on 11/13/23.
+//
+
+import UIKit
+
+class detailThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
